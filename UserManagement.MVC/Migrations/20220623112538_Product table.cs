bb180f541060f214492ebccf1470938b7e702ffd
Ivan Serdyukov <ivan.serdyukov270@gmail.com>
2022-06-23 14:58:38 +0300

    Add Products (MVC + migration + AppDBContext + UserManagement.MVC.csproj(Add))

---
+++ UserManagement.MVC/Migrations/20220623112538_Product table.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UserManagement.MVC.Migrations
+{
+    public partial class Producttable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                schema: "Identity",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.ProductId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product",
+                schema: "Identity");
+        }
+    }
+}
